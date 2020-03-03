c5aebc81116e916da66d736ba0044165d29468b2
syter10086 <skyter10086@aliyun.com>
2020-03-04 01:47:02 +0800

    Add database support.

--- app.py
+++ app.py
@@ -1,6 +1,67 @@
 from flask import Flask, escape, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+import click
+import os
+import sys
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #关闭对模型变动的监控
+db = SQLAlchemy(app)
+
+
+@app.cli.command() # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.') # 设置选项
+def initdb(drop):
+    # Initialize the database
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialize the database.')
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+    
+    name = 'TzengBoyan'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done.')
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
 
 name = 'TzengBoyan'
 movies = [
@@ -35,5 +96,8 @@ def test_url_for():
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+
+    return render_template('index.html', user=user, movies=movies)
 
